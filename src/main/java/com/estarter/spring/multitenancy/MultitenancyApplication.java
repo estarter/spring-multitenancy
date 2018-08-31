79bfa49948781cdee4e2a97dd5985171a02083e9
estarter <estarter@gmail.com>
2018-08-31 23:10:57 +0200

    init

---
+++ src/main/java/com/estarter/spring/multitenancy/MultitenancyApplication.java
@@ -0,0 +1,12 @@
+package com.estarter.spring.multitenancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitenancyApplication.class, args);
+	}
+}
